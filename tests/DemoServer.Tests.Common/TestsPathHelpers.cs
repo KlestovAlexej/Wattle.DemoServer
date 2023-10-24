0355ef5e882df189a3b01d7ba66a9af5bf487ed2
klestov <klestov@hotmail.com>
2023-10-24 22:00:20 +0300

    test

---
+++ tests/DemoServer.Tests.Common/TestsPathHelpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using ShtrihM.DemoServer.Common;
+using ShtrihM.Wattle3.Testing;
+using BaseTests = ShtrihM.DemoServer.Testing.BaseTests;
+
+namespace ShtrihM.DemoServer.Tests.Common;
+
+[TestFixture]
+public class TestsPathHelpers : BaseTests
+{
+    [Test]
+    [Timeout(TestTimeout.Unit)]
+    [Category(TestCategory.Unit)]
+    public void Test()
+    {
+        var basePath = new DirectoryInfo(Path.GetDirectoryName(typeof(TestsPathHelpers).Assembly.Location)!).FullName;
+        var path = PathHelpers.GetBasedPath(basePath, "TestsPathHelpers.cs");
+
+        Console.WriteLine(path);
+        Assert.AreEqual(Path.Combine(basePath, "TestsPathHelpers.cs"), path);
+    }
+}
