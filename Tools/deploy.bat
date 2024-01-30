c45668a52837341a1eb4240aaf145bb1fc1e803c
KlestovAlexej <klestov.alexej@outlook.com>
2024-01-30 15:46:57 +0300

    build

--- Tools/deploy.bat
+++ Tools/deploy.bat
@@ -13,6 +13,34 @@ IF %ERRORLEVEL% NEQ 0 EXIT 1
 mkdir Deploy\Test\Processing\bin\Debug
 IF %ERRORLEVEL% NEQ 0 EXIT 1
 
+rem --- BUILD_TEST -----------------
+
+rmdir src\DemoServer.Processing.Application\bin /s /q
+IF %ERRORLEVEL% NEQ 0 EXIT 1
+
+rmdir src\DemoServer.Processing.Application\obj /s /q
+IF %ERRORLEVEL% NEQ 0 EXIT 1
+
+dotnet build Tools\MSBuild\Publish.csproj "/p:BuildProfile=/p:DefineConstants=BUILD_TEST" "/p:DefineConstants=BUILD_TEST"
+IF %ERRORLEVEL% NEQ 0 EXIT 1
+
+xcopy Publish\ProductDescription.xml Deploy\Test\
+IF %ERRORLEVEL% NEQ 0 EXIT 1
+
+xcopy src\DemoServer.Processing.Application\WindowsRegister.txt Deploy\Test\Processing\
+IF %ERRORLEVEL% NEQ 0 EXIT 1
+
+xcopy src\DemoServer.Processing.DataAccess.Postgresql\DemoServer.Processing.sql Deploy\Test\Processing\
+IF %ERRORLEVEL% NEQ 0 EXIT 1
+
+xcopy src\DemoServer.Processing.Application\bin\Release\net8.0-windows\win-x64\ Deploy\Test\Processing\bin\Release\
+IF %ERRORLEVEL% NEQ 0 EXIT 1
+
+xcopy src\DemoServer.Processing.Application\bin\Debug\net8.0-windows\win-x64\ Deploy\Test\Processing\bin\Debug\
+IF %ERRORLEVEL% NEQ 0 EXIT 1
+
+rem --- BUILD_PROD -----------------
+
 rmdir src\DemoServer.Processing.Application\bin /s /q
 IF %ERRORLEVEL% NEQ 0 EXIT 1
 
@@ -43,28 +71,7 @@ IF %ERRORLEVEL% NEQ 0 EXIT 1
 xcopy src\DemoServer.Processing.Application\bin\Debug\net8.0-windows\win-x64\ Deploy\Prod\Processing\bin\Debug\
 IF %ERRORLEVEL% NEQ 0 EXIT 1
 
-rmdir src\DemoServer.Processing.Application\bin /s /q
-IF %ERRORLEVEL% NEQ 0 EXIT 1
-
-rmdir src\DemoServer.Processing.Application\obj /s /q
-IF %ERRORLEVEL% NEQ 0 EXIT 1
-
-dotnet build Tools\MSBuild\Publish.csproj "/p:BuildProfile=/p:DefineConstants=BUILD_TEST" "/p:DefineConstants=BUILD_TEST"
-IF %ERRORLEVEL% NEQ 0 EXIT 1
-
-xcopy Publish\ProductDescription.xml Deploy\Test\
-IF %ERRORLEVEL% NEQ 0 EXIT 1
-
-xcopy src\DemoServer.Processing.Application\WindowsRegister.txt Deploy\Test\Processing\
-IF %ERRORLEVEL% NEQ 0 EXIT 1
-
-xcopy src\DemoServer.Processing.DataAccess.Postgresql\DemoServer.Processing.sql Deploy\Test\Processing\
-IF %ERRORLEVEL% NEQ 0 EXIT 1
-
-xcopy src\DemoServer.Processing.Application\bin\Release\net8.0-windows\win-x64\ Deploy\Test\Processing\bin\Release\
-IF %ERRORLEVEL% NEQ 0 EXIT 1
-
-xcopy src\DemoServer.Processing.Application\bin\Debug\net8.0-windows\win-x64\ Deploy\Test\Processing\bin\Debug\
+echo Сборка успешно выполнена > Publish/ReadMe.txt
 IF %ERRORLEVEL% NEQ 0 EXIT 1
 
 echo Сборка успешно выполнена > Deploy/ReadMe.txt
